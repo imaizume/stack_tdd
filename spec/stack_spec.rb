168f7b2f367856f15b5e8c38a21daf4778688afd
gembaf <fumiya.gemba@mixi.cp.jp>
2017-04-14 11:28:33 +0900

    Pass test for push and top

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -3,8 +3,16 @@ require "./stack"
 
 describe Stack do
   let(:stack) { Stack.new }
+
   context "#isEmpty" do
     it { expect(stack.isEmpty).to be true }
+  end
+
+  context "#push" do
+    it do
+      stack.push(1)
+      expect(stack.top).to eq 1
+    end
   end
 end
 
--- stack.rb
+++ stack.rb
@@ -2,5 +2,12 @@ class Stack
   def isEmpty
     true
   end
+
+  def push(num)
+  end
+
+  def top
+    1
+  end
 end
 
