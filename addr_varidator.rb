14b4d84295361b68b06ff22ee8581c4f2283250f
gembaf <fumiya.gemba@mixi.cp.jp>
2017-04-14 15:43:09 +0900

    Implement AddrVaridator#check_local_part

--- addr_varidator.rb
+++ addr_varidator.rb
@@ -2,4 +2,8 @@ module AddrVaridator
   def split(s)
     s.split("@", 2)
   end
+
+  def check_local_part(str)
+    str.match(/@|\\|\s|　/).nil?
+  end
 end
--- spec/addr_varidator_spec.rb
+++ spec/addr_varidator_spec.rb
@@ -9,5 +9,21 @@ describe AddrVaridator do
     it { expect(split("a@b@c")).to eq %w(a b@c) }
     it { expect(split("abc")).to eq %w(abc) }
   end
+
+  context "#check_local_part" do
+    context '正しい入力の場合' do
+      it { expect(check_local_part("abc")).to be true }
+    end
+
+    context '全角文字が含まれている場合' do
+      it { expect(check_local_part("a　c")).to be false }
+    end
+
+    context '禁止文字が含まれている場合' do
+      it { expect(check_local_part("a@c")).to be false }
+      it { expect(check_local_part("a\\c")).to be false }
+      it { expect(check_local_part("a c")).to be false }
+    end
+  end
 end
 
