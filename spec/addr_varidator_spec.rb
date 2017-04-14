88eb37fb142bfd63a8930e78706c1d0027b0d0ff
gembaf <fumiya.gemba@mixi.cp.jp>
2017-04-14 16:12:56 +0900

    Refactoring

--- spec/addr_varidator_spec.rb
+++ spec/addr_varidator_spec.rb
@@ -10,21 +10,26 @@ describe AddrVaridator do
     it { expect(split("abc")).to eq %w(abc) }
   end
 
-  context "#check_local_part" do
+  context "#include_ng_char?" do
     context '正しい入力の場合' do
       it { expect(include_ng_char?("abc")).to be true }
-      it { expect(include_zenkaku?("abc")).to be true }
-    end
-
-    context '全角文字が含まれている場合' do
-      it { expect(include_zenkaku?("a　c")).to be false }
-      it { expect(include_zenkaku?("あいうえお")).to be false }
     end
 
     context '禁止文字が含まれている場合' do
       it { expect(include_ng_char?("a@c")).to be false }
       it { expect(include_ng_char?("a\\c")).to be false }
       it { expect(include_ng_char?("a c")).to be false }
+    end
+  end
+
+  context "#include_zenkaku?" do
+    context '正しい入力の場合' do
+      it { expect(include_zenkaku?("abc")).to be true }
+    end
+
+    context '全角文字が含まれている場合' do
+      it { expect(include_zenkaku?("a　c")).to be false }
+      it { expect(include_zenkaku?("あいうえお")).to be false }
     end
   end
 end
